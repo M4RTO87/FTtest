d5222a433b46e7f86d52982f78d61407c2cb04de
M4RTO87 <mi.castillo875@gmail.com>
2025-01-14 21:53:34 -0300

    Add files via upload

---
+++ def.py
@@ -0,0 +1,49 @@
+from os import system
+system("cls")
+
+# def suma():
+#     nu1=int(input("N° "))
+#     nu2=int(input("N° "))
+#     print(nu1+nu2)
+    
+# def res():
+#     nu1=int(input("N° "))
+#     nu2=int(input("N° "))
+#     print(nu1-nu2)
+    
+# def mul():
+#     nu1=int(input("N° "))
+#     nu2=int(input("N° "))
+#     print(nu1*nu2)
+    
+# def div():
+#     nu1=int(input("N° "))
+#     nu2=int(input("N° "))
+#     print(nu1/nu2)
+    
+    
+# while True:
+#     print("""
+#           1.- suma
+#           2.- resta
+#           3.- multi
+#           4.- divi
+#           5.- salir""")
+#     op=int(input(">>> "))
+#     match op:
+#         case 1:
+#             suma()
+#         case 2:
+#             res()
+#         case 3:
+#             mul()
+#         case 4:
+#             div()
+#         case 5:
+#             break
+#         case _:print("invalid")
+
+# num=int(input(">>> "))
+# for i in range(num+1):
+#     print("*"*((i+1)-1))
+
